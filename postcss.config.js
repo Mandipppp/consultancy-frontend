245676c31f77e9670c390fec9321019fe66ed661
bishalKoi <20029260@students.koi.edu.au>
2025-05-25 23:18:25 +0545

    chore: switch to @tailwindcss/postcss plugin and ESM config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+import postcssTailwind from '@tailwindcss/postcss'
+import autoprefixer     from 'autoprefixer'
+
+export default {
+  plugins: [
+    postcssTailwind(),    // ← uses the new @tailwindcss/postcss package
+    autoprefixer(),       // ← autoprefixer as usual
+  ],
 }
